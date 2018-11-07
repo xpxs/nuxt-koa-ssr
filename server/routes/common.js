8ac56989335c487cf95bbcaff1880fafa68b7be4
junner8909 <908900776@qq.com>
2018-11-07 19:03:27 +0800

    新增公共接口

---
+++ server/routes/common.js
@@ -0,0 +1,13 @@
+import Router from 'koa-router'
+import { resDataTpl } from '../controller/resDataTpl' // 引入返回数据模板
+
+const router = Router()
+
+// 用户列表
+router.get('/getMsg', async (ctx, next) => {
+  resDataTpl.success = true
+  resDataTpl.message = '发送验证码成功'
+  ctx.body = resDataTpl
+})
+
+export default router
