80a8a0c5a0c4615162617a5ece561235dfe4bbe1
junner8909 <908900776@qq.com>
2018-11-15 20:22:20 +0800

    新增加载与分页控件

---
+++ client/components/common/Pagetion.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-box">
+    <Page 
+      :total="params.total"
+      :size="params.size"
+      :show-elevator="params.showElevator"
+      :show-sizer="params.showSizer"
+      :show-total="params.showTotal" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    params: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.page-box {
+  padding: 10px;
+  background: #fff;
+}
+</style>
---
+++ client/components/common/ReloadSpin.vue
@@ -0,0 +1,19 @@
+<template>
+  <Spin 
+    v-if="spinShow"
+    size="large"
+    fix/>
+</template>
+<script>
+export default {
+  props: {
+    spinShow: {
+      type: Boolean,
+      required: true,
+      default() {
+        return false
+      }
+    }
+  }
+}
+</script>
